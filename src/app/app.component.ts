1303c90ed2d928a57504775a1640ae2b01bb72c9
DaveTorrez <davidtn52@gmail.com>
2024-12-27 01:27:30 -0400

    separa en componentes las pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AsidebarComponent } from './shared/asidebar/asidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, AsidebarComponent, FooterComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
